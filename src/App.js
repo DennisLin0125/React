8f2cd625abe6bf1438d2496b26f9b423d1e405a3
Dennis <740125aa@gmail.com>
2023-09-24 17:09:59 +0800

    基本配置

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-// 創建外殼組件App
+import React, { Component } from 'react'
 
-// 引入React 核心庫
-import React, { Component } from 'react';
-// 引入Hello組件
-import Hello from './componets/Hello/Hello.js';
-// 引入Welcome組件
-import Welcome from './componets/Welcome/Welcome.js';
-
-
-class App extends Component {
-    state = {  }
-    render() {
-        return (
-            <div>
-               <Hello/>
-               <Welcome/>
-            </div>
-        );
-    }
+export default class App extends Component {
+  render() {
+    return (
+      <div>App.....</div>
+    )
+  }
 }
-
-export default App;
