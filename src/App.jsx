6f41ab82c6f993cb07f958eb8caf34c6934b43be
Dennis <740125aa@gmail.com>
2023-09-24 11:03:55 +0800

    動態初始化列表

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,23 @@ import Footer from "./componets/Footer"
 import "./App.css"
 
 export default class App extends Component {
+
+  // 初始化狀態
+  state={ todos: [
+    {id: '001',name: '吃飯', done: true},
+    {id: '002',name: '睡覺', done: true},
+    {id: '003',name: '打程式', done: false},
+    {id: '004',name: '逛街', done: true},
+  ]}
+
   render() {
+    const {todos} = this.state;
+
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header/>
-          <List/>
+          <List todos={todos}/>
           <Footer/>
         </div>
       </div>
--- src/componets/List/index.jsx
+++ src/componets/List/index.jsx
@@ -4,10 +4,18 @@ import "./index.css"
 import Item from "../Item"
 
 export default class List extends Component {
+
   render() {
+
+    const {todos} = this.props
     return (
       <ul className="todo-main">
-        <Item />
+        {
+          todos.map((todo) => {
+            // return <Item key={todo.id} id={todo.id} name={todo.name} done={todo.done}/>;
+            return <Item key={todo.id} {...todo}/>;
+          })
+        }
       </ul>
     )
   }
