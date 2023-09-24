fd7619088c8124f8933b5de367062f7f3c21cd5d
Dennis <740125aa@gmail.com>
2023-09-24 17:38:02 +0800

    React_axios基本配置

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
 
+import axios from 'axios'
+
+// 配置defaul url
+axios.defaults.baseURL = "http://localhost:5000"
+
 export default class App extends Component {
+  
+  getStudentData = () => {
+    axios({
+        method: "GET",
+        //url
+        url: "/students",
+
+    }).then(res => {
+        console.log("請求成功:",res);
+    }).catch((reason)=>{
+        console.error("失敗了:",reason);
+    })
+  }
   render() {
     return (
-      <div>App.....</div>
+      <div>
+        <button onClick={this.getStudentData}>點擊獲取學生訊息</button>
+      </div>
     )
   }
 }
