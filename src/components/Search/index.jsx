32fde2d3bebf745b030b9f637d976a705b49f646
Dennis <740125aa@gmail.com>
2023-09-25 20:44:34 +0800

    github搜索案例 axios發送請求
    需訪問port:5000的server
    
    故需配置proxy於port3000,再讓proxy去訪問port:5000的server

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Search extends Component {
+
+  handleSearch = () => {
+    // 獲取用戶輸入,並改名為keyWord
+    const {keyWordElement:{value:keyWord}} = this;
+    // 發送網路請求  發送給代理伺服器
+    axios.get(`/api1/search/users?q=${keyWord}`)
+    .then((respones) => {
+      console.log('respons', respones.data)
+    })
+    .catch((err) => {
+      console.log('err:', err)
+    })
+  }
+
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">Search Github Users</h3>
         <div>
-          <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+          <input ref={c => {this.keyWordElement = c}} type="text" placeholder="enter the name you search"/>&nbsp;
+          <button onClick={this.handleSearch}>Search</button>
         </div>
       </section>
     )
