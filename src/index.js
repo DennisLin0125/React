9e0f8d094284a97c31e70b02096fef7d068eadbc
Dennis <740125aa@gmail.com>
2023-09-26 23:23:19 +0800

    React Router 一級路由+SPA+靜態配置

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 import React from 'react';
 // 引入ReactDOM
 import ReactDOM from 'react-dom/client';
+// 引入Router
+import {BrowserRouter} from 'react-router-dom'
 // 引入App組件
 import App from "./App"
 
 // 渲染app組件到頁面
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
+    </BrowserRouter>
   );
