6af04395ae38dd5418771855840a814adc304ad2
Dennis <740125aa@gmail.com>
2023-09-26 20:54:52 +0800

    github搜索案例 fetch發送請求
    還有使用async await

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import PubSub from 'pubsub-js'  // npm install pubsub-js
-import axios from 'axios'
+// import axios from 'axios'
 
 export default class Search extends Component {
 
-  handleSearch = () => {
+  handleSearch = async () => {
 
     // 獲取用戶輸入,並改名為keyWord
     const {keyWordElement:{value:keyWord}} = this;
@@ -21,21 +21,56 @@ export default class Search extends Component {
       isLoading: true
     });
     // 發送網路請求  發送給代理伺服器
-    axios.get(`/api1/search/users?q=${keyWord}`)
-    .then((respones) => {
-      // 請求成功後通知List更新狀態
+    // axios.get(`/api1/search/users?q=${keyWord}`)
+    // .then((respones) => {
+    //   // 請求成功後通知List更新狀態
+    //   PubSub.publish('hello',{
+    //     isLoading: false,
+    //     users:respones.data.items
+    //   });
+    // })
+    // .catch((err) => {
+    //   // 請求失敗後通知List更新狀態
+    //   PubSub.publish('hello',{
+    //     isLoading: false,
+    //     errMsg:err.message
+    //   });
+    // })
+
+    // 使用fetch
+    // fetch(`/api1/search/users2?q=${keyWord}`)
+    // .then((respones) => {
+    //   console.log('連接server正常')
+    //   return respones.json()
+    // })
+    // .then((respones) => {
+    //   PubSub.publish('hello',{
+    //     isLoading: false,
+    //     users:respones.items
+    //   });
+    // })
+    // .catch((err) => {
+    //   PubSub.publish('hello',{
+    //     isLoading: false,
+    //     errMsg:err.message
+    //   });
+    // })
+
+    // 使用async await
+    try {
+      const respones = await fetch(`/api1/search/users2?q=${keyWord}`);
+      const data = await respones.json();
       PubSub.publish('hello',{
-        isLoading: false,
-        users:respones.data.items
-      });
-    })
-    .catch((err) => {
-      // 請求失敗後通知List更新狀態
+            isLoading: false,
+            users:data.items
+          });
+
+    } catch (err) {
       PubSub.publish('hello',{
-        isLoading: false,
-        errMsg:err.message
+            isLoading: false,
+            errMsg:err.message
       });
-    })
+    }
   }
 
   render() {
