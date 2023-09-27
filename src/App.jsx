e93da5a9f572335bd1acb6e120ad259c0c61735e
Dennis <740125aa@gmail.com>
2023-09-27 19:21:33 +0800

    React Router  讓導航根據自訂的CSS高亮

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import About from './pages/About'
 import Home from './pages/Home'
 
 export default function App() {
+  function computeClassName({isActive}){
+    return isActive ? "list-group-item selected" : "list-group-item"
+  }
   return (
     <div>
       <div className="row">
@@ -15,8 +18,8 @@ export default function App() {
         <div className="col-xs-2 col-xs-offset-2">
           <div className="list-group">
             {/* 路由鏈接 */}
-            <NavLink className="list-group-item" to="/about">About</NavLink>
-            <NavLink className="list-group-item" to="/home">Home</NavLink>
+            <NavLink className={computeClassName} to="/about">About</NavLink>
+            <NavLink className={computeClassName} to="/home">Home</NavLink>
           </div>
         </div>
         <div className="col-xs-6">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
-import React , {useState} from 'react'
-import { Navigate } from 'react-router-dom';
+import React  from 'react'
 
 export default function Home() {
-  const [sum,setSum] = useState(1);
   return (
     <div>
       <h3>我是Home的内容</h3>
-      { sum === 2 ? <Navigate to="/about"/> : <h4>當前Sum的值為 { sum }</h4> }
-      <button onClick={() => setSum(2)}>點我將Sum變為2</button>
     </div>
   )
 }
