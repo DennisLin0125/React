97c68e2b34a2801c8c17f9a48a19c8cc32c79d59
Dennis <740125aa@gmail.com>
2023-09-29 16:14:12 +0800

    antd 基本使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Button, DatePicker } from 'antd';  //npm i antd
+import { WechatOutlined, SearchOutlined } from '@ant-design/icons';  // npm install @ant-design/icons --save
 
 export default function App() {
+  const onChange = (date, dateString) => {
+    console.log(date, dateString);
+  };
   return (
-    <h1>App</h1>
+    <div>App....
+      <Button type="primary">primary按鈕</Button>
+      <Button type="link">link按鈕</Button>
+      <Button type="primary" icon={<SearchOutlined />}>
+        Search
+      </Button>
+      <WechatOutlined />
+
+      <DatePicker onChange={onChange} />
+    </div>
   )
 }
