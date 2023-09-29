7a8cf16e4b97c533d9068f57ebc6636e7d991d2c
Dennis <740125aa@gmail.com>
2023-09-29 11:07:44 +0800

    給定一個URL 利用useResolvedPath() 來解析裡面的 path search hash

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { useNavigationType } from 'react-router-dom'
+import { useNavigationType, useResolvedPath } from 'react-router-dom'
 
 export default function News() {
     console.log('useNavigationType', useNavigationType())
+    console.log('useResolvedPath', useResolvedPath('/user?id=001&name=dennis#qwer'))
+
     return (
         <ul>
             <li>news001</li>
