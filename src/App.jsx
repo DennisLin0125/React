e286a14784e7cb63e2c75b0fd79f90da760fa7ce
Dennis <740125aa@gmail.com>
2023-10-01 14:24:30 +0800

    優化 2 Provider组件的使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import Count from './containers/count'
 
-// 引入store
-import store from './redux/store'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Count store={store}/>
+        <Count />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -6,20 +6,15 @@ import ReactDOM from 'react-dom/client';
 import App from "./App"
 
 import store from './redux/store'
+import {Provider} from 'react-redux'
+
 
 // 渲染app組件到頁面
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   );
-
-  // 監測redux中的狀態變化,只要變化,就調用render
-store.subscribe(() => {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );  
-})
