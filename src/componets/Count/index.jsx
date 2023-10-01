97a4cd4cf3b0507d3d2d6b256086d26e817db116
Dennis <740125aa@gmail.com>
2023-10-01 12:55:42 +0800

    基本應用

--- src/componets/Count/index.jsx
+++ src/componets/Count/index.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 
-
-
 export default class Count extends Component {
 
     state = { carName: "BMW" }
@@ -10,27 +8,34 @@ export default class Count extends Component {
     increment = () => {
         // 獲取用戶輸入
         const { value } = this.selectNumber;
+        this.props.add(value * 1);
     }
     // 減法
     decrement = () => {
         // 獲取用戶輸入
         const { value } = this.selectNumber;
+        this.props.sub(value * 1);
     }
     // 當前求和為奇數再加
     incrementIfOdd = () => {
         // 獲取用戶輸入
         const { value } = this.selectNumber;
+        if (this.props.count % 2 !== 0) {
+            this.props.add(value * 1);
+        }
     }
     // 異步加
     incrementAsync = () => {
         // 獲取用戶輸入
         const { value } = this.selectNumber;
+        this.props.async(value * 1, 500);
 
     }
     render() {
+        console.log('UI:', this.props)
         return (
             <div>
-                <h1>當前求和為: {}</h1>
+                <h1>當前求和為: {this.props.count}</h1>
                 <select ref={c => this.selectNumber = c}>
                     <option value="1">1</option>
                     <option value="2">2</option>
--- src/containers/count/index.jsx
+++ src/containers/count/index.jsx
@@ -1,8 +1,26 @@
 // 引入count的UI組件
 import CountUI from '../../componets/Count'
 // 引入connect
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
-const CountContainer = connect()(CountUI);
+import {
+    createIncrementAction,
+    createDecrementAction,
+    createIncrementAsyncAction
+} from '../../redux/count_action'
+
+// mapStateToProbs函數的返回值作為狀態值傳給了UI組件
+function mapStateToProbs(state) {
+    return { count: state.count }
+}
+function mapDispatchToProbs(dispatch) {
+    return {
+        add: (data) => dispatch(createIncrementAction(data)),
+        sub: (data) => dispatch(createDecrementAction(data)),
+        async: (data, time) => dispatch(createIncrementAsyncAction(data, time)),
+    }
+}
+
+const CountContainer = connect(mapStateToProbs, mapDispatchToProbs)(CountUI);
 
 export default CountContainer;
