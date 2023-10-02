3d0e717a2f27cfd2efae35a23f72f2c48a7c2aaf
Dennis <740125aa@gmail.com>
2023-10-02 19:48:09 +0800

    擴展  setState 方式

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
+import Demo from './componets/01_setState'
+
 export default class App extends Component {
   render() {
     return (
-      <div>App.....</div>
+      <div>
+        <Demo x={1}/>
+      </div>
     )
   }
 }
